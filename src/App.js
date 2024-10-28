33edd02537449b13bb69706c38760aac245e204e
AnshBudhiraja7 <145669812+AnshBudhiraja7@users.noreply.github.com>
2024-10-28 16:39:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import React, { useState } from 'react'
-import imageCompression from 'browser-image-compression'
 const App = () => {
-  const [image,setimage]=useState({})
-  const[img,setimg]=useState(null)
-  async function upload(event)
-  {
-    var file=event.target.files[0]
-    if(file)
-    {      
-      const options = {
-        maxSizeMB: 2, // Maximum size in MB
-        maxWidthOrHeight: 300,
-        useWebWorker: true,
-      }
-      var compressedfile= await imageCompression(file,options)
-      setimage(compressedfile);
-      var result= URL.createObjectURL(compressedfile)
   
-      setimg(result);
-    }
-  }
   return (
     <div className='container'>
-      <input type="file" onChange={upload} className="form-control" />
-      <button className='btn btn-success'>Save</button>
-
-      <img src={img} alt="" />
-      
+    hello world
     </div>
   )
 }

ac3d71c24b94037d8d37f4ab33fc74afd2274b28
AnshBudhiraja7 <Anshbudhiraja72@gmail.com>
2024-10-28 22:33:24 +0530

    New update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
-import React, { useState } from 'react'
-import imageCompression from 'browser-image-compression'
+import React from 'react'
+import Array from './Array'
+import "./Style.css"
 const App = () => {
-  const [image,setimage]=useState({})
-  const[img,setimg]=useState(null)
-  async function upload(event)
-  {
-    var file=event.target.files[0]
-    if(file)
-    {      
-      const options = {
-        maxSizeMB: 2, // Maximum size in MB
-        maxWidthOrHeight: 300,
-        useWebWorker: true,
-      }
-      var compressedfile= await imageCompression(file,options)
-      setimage(compressedfile);
-      var result= URL.createObjectURL(compressedfile)
-  
-      setimg(result);
-    }
-  }
   return (
-    <div className='container'>
-      <input type="file" onChange={upload} className="form-control" />
-      <button className='btn btn-success'>Save</button>
-
-      <img src={img} alt="" />
+    <div>
+    {
+      Array.map(function(obj){
+        return(
+          <div className='row'>
+            <img src={obj.Path} alt="" />
+            <p>{obj.Item}</p>
+            <p>₹{obj.Price}/-</p>
+          </div>
+        )
+        
+      })
       
+    }    
     </div>
   )
 }
---
+++ src/Array.js
@@ -0,0 +1,198 @@
+var Array=[
+    {
+    Item:"Pizza",
+    Category:"Snacks",
+    Path:"Images/pizza.jpg",
+    Price:"160",
+    },{
+    Item:"Chowmein",
+    Category:"Snacks",
+    Path:"Images/Chowmein.jpg",
+    Price:"90",
+    },{
+    Item:"Honey Chilli Potato",
+    Category:"Snacks",
+    Path:"Images/Honey Chilli Potato.jpg",
+    Price:"180",
+    },{
+    Item:"Momos",
+    Category:"Snacks",
+    Path:"Images/momos.jpg",
+    Price:"70",
+    },{
+    Item:"Maggi",
+    Category:"Snacks",
+    Path:"Images/maggi.jpeg",
+    Price:"30",
+    },{
+    Item:"Burger",
+    Category:"Snacks",
+    Path:"Images/burger.jpg",
+    Price:"40",
+    },{
+    Item:"Fries",
+    Category:"Snacks",
+    Path:"Images/fries.jpeg",
+    Price:"50",
+    },{
+    Item:"Spring Rolls",
+    Category:"Snacks",
+    Path:"Images/Spring-Roll.jpg",
+    Price:"60",
+    },{
+    Item:"Masala Dosa",
+    Category:"Course",
+    Path:"Images/Masala-Dosa.jpg",
+    Price:"110",
+    },{
+    Item:"Chole Bhature",
+    Category:"Course",
+    Path:"Images/chole bhature.jpg",
+    Price:"120",
+    },{
+    Item:"Puri Aloo",
+    Category:"Course",
+    Path:"Images/purialoo.jpg",
+    Price:"120",
+    },{
+    Item:"Pav Bhaji",
+    Category:"Course",
+    Path:"Images/pavbhaji.jpg",
+    Price:"110",
+    },{
+    Item:"Dal Makhni",
+    Category:"Course",
+    Path:"Images/Dal-Makhani.jpg",
+    Price:"180",
+    },{
+    Item:"Yellow Dal",
+    Category:"Course",
+    Path:"Images/yellow dal.jpg",
+    Price:"170",
+    },{
+    Item:"Dal Tadka",
+    Category:"Course",
+    Path:"Images/Dal-Tadka.jpg",
+    Price:"160",
+    },{
+    Item:"Shahi Paneer",
+    Category:"Course",
+    Path:"Images/Shahi-Paneer-1.jpg",
+    Price:"220",
+    },{
+    Item:"Palak Paneer",
+    Category:"Course",
+    Path:"Images/palakpaneer.jpg",
+    Price:"240",
+    },{
+    Item:"Paneer Korma",
+    Category:"Course",
+    Path:"Images/paneer korma.jpg",
+    Price:"270",
+    },{
+    Item:"Aloo Gobhi",
+    Category:"Course",
+    Path:"Images/aloo-gobi.jpg",
+    Price:"190",
+    },{
+    Item:"Aloo Mutter",
+    Category:"Course",
+    Path:"Images/aloo mutter.jpg",
+    Price:"190",
+    },{
+    Item:"Tandoori Roti",
+    Category:"Indian Bread",
+    Path:"Images/Tandoori-Roti.jpg",
+    Price:"10",
+    },{
+    Item:"Butter Roti",
+    Category:"Indian Bread",
+    Path:"Images/butterroti.jpg",
+    Price:"12",
+    },{
+    Item:"Onion Roti",
+    Category:"Indian Bread",
+    Path:"Images/onion roti.jpg",
+    Price:"17",
+    },{
+    Item:"Butter Naan",
+    Category:"Indian Bread",
+    Path:"Images/Butter-Naan.jpg",
+    Price:"45",
+    },{
+    Item:"Plain Naan",
+    Category:"Indian Bread",
+    Path:"Images/plainnaa.jpg",
+    Price:"40",
+    },{
+    Item:"Stuffed Naan",
+    Category:"Indian Bread",
+    Path:"Images/Stuffednaan.jpg",
+    Price:"50",
+    },{
+    Item:"Missi Roti",
+    Category:"Indian Bread",
+    Path:"Images/Missi.jpg",
+    Price:"25",
+    },{
+    Item:"Plain Rice",
+    Category:"Rice",
+    Path:"Images/plainrice.jpg",
+    Price:"100"
+    },{
+    Item:"Zeera Rice",
+    Category:"Rice",
+    Path:"Images/zeerarice.jpeg",
+    Price:"120"
+    },{
+    Item:"Mutter Pullao",
+    Category:"Rice",
+    Path:"Images/mutterpullao.jpg",
+    Price:"130"
+    },{
+    Item:"Veg. Pullao",
+    Category:"Rice",
+    Path:"Images/vegpullao.jpg",
+    Price:"150"
+    },{
+    Item:"Paneer Pullao",
+    Category:"Rice",
+    Path:"Images/paneerpullao.jpg",
+    Price:"200"
+    },{
+    Item:"Veg. Biryani",
+    Category:"Rice",
+    Path:"Images/veg biryani.jpg",
+    Price:"190"
+    },{
+    Item:"Kashmiri Pullao",
+    Category:"Rice",
+    Path:"Images/kashmiri.pulao.jpg",
+    Price:"220"
+    },{
+    Item:"Tilla Kulfi",
+    Category:"Desert",
+    Path:"Images/Tilla.jpg",
+    Price:"40",
+    },{
+    Item:"Vanilla Cup",
+    Category:"Desert",
+    Path:"Images/vanilla.jpg",
+    Price:"75",
+    },{
+    Item:"Butter Scotch Cup",
+    Category:"Desert",
+    Path:"Images/butter scotch.jpg",
+    Price:"80",
+    },{
+    Item:"Casata Cup",
+    Category:"Desert",
+    Path:"Images/casata.jpg",
+    Price:"90",
+    },{
+    Item:"Gulab Jamun",
+    Category:"Desert",
+    Path:"Images/gulab jamun.JPG",
+    Price:"15/pc",
+    }]
+    export default Array
